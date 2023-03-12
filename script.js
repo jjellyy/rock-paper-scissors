33d179140160e92ce7a2286ba2dd974db0db10e5
jjellyy <3197941+jjellyy@users.noreply.github.com>
2023-03-12 23:07:59 +0000

    Add final score and winner display

--- script.js
+++ script.js
@@ -13,20 +13,6 @@ function getComputerChoice () {
     }
 }
 
-/*
-function getPlayerChoice() {
-    let choice = window.prompt("Rock, paper or scissors? Make your choice.")
-    choice = choice.toLowerCase().trim();
-    while (true) {
-        if (choice == "rock" || choice == "scissors" || choice =="paper") {
-            break;
-        }
-        choice = window.prompt("Please choose rock, paper or scissors.")
-    }
-    return choice;
-}
-*/
-
 function getRandom () {
     const floatRandom = Math.random()
     const difference = 2
@@ -36,9 +22,7 @@ function getRandom () {
     return randomWithinRange;
 }
 
-function playRound(e) {
-    const playerChoice = e.target.id;
-    const computerChoice = getComputerChoice();
+function playRound(playerChoice, computerChoice) {
     if (playerChoice == computerChoice) {
         results.innerHTML = `Computer chose ${computerChoice}.<br>
         It's a draw!`
@@ -48,66 +32,78 @@ function playRound(e) {
             case "rock":
                 switch (computerChoice) {
                     case "scissors":
+                        playerScore++;
                         results.innerHTML = `Computer chose ${computerChoice}.<br> 
-                        You win! Rock beats scissors.`
+                        You win! Rock beats scissors.<br>
+                        Player: ${playerScore}<br>
+                        Computer: ${computerScore}`                        
                         return "win";
                     case "paper":
+                        computerScore++;
                         results.innerHTML = `Computer chose ${computerChoice}.<br>
-                        You lose! Paper beats rock.`
+                        You lose... Paper beats rock.<br>
+                        Player: ${playerScore}<br>
+                        Computer: ${computerScore}`  
                         return "lose";              
                 }
                 break;
             case "paper":
                 switch (computerChoice) {
                     case "rock":
+                        playerScore++;
                         results.innerHTML = `Computer chose ${computerChoice}.<br>
-                        You win! Paper beats rock.`
+                        You win! Paper beats rock.<br>
+                        Player: ${playerScore}<br>
+                        Computer: ${computerScore}`  
                         return "win";
                     case "scissors":
+                        computerScore++;
                         results.innerHTML = `Computer chose ${computerChoice}.<br>
-                        You lose! Scissors beat paper.`
+                        You lose... Scissors beat paper.<br>
+                        Player: ${playerScore}<br>
+                        Computer: ${computerScore}`  
                         return "lose";  
                 }
             case "scissors":
                 switch (computerChoice) {
                     case "rock":
+                        computerScore++;
                         results.innerHTML = `Computer chose ${computerChoice}.<br>
-                        You lose! Rock beats scissors.`
+                        You lose... Rock beats scissors.<br>
+                        Player: ${playerScore}<br>
+                        Computer: ${computerScore}`  
                         return "lose";  
                     case "paper":
+                        playerScore++;
                         results.innerHTML = `Computer chose ${computerChoice}.<br>
-                        "You win! Scissors beats paper.`
+                        You win! Scissors beats paper.<br>
+                        Player: ${playerScore}<br>
+                        Computer: ${computerScore}`  
                         return "win";
                 }
         }
     }
 }
-/*
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let result;
-    for (let i = 0; i < 5; i++) {
-        result = playRound(getPlayerChoice(), getComputerChoice());
-        if (result == "win") {
-            playerScore++;
-        } else if (result == "lose") {
-            computerScore++;
-        } else {
 
-        }        
-    }
-    if (playerScore > computerScore) {
-        console.log("You win " + playerScore + " to " + computerScore)
-    } else if (computerScore > playerScore) {
-        console.log("You lose " + playerScore + " to " + computerScore)
-    } else {
-        console.log("It's a draw")
+function game(e) {
+    let playerChoice = e.target.id;
+    playRound(playerChoice, getComputerChoice());
+    if (playerScore >= 5) {
+        results.innerHTML += `<br>You win ${playerScore} to ${computerScore}!`
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore >= 5) {
+        results.innerHTML += `<br>lose ${playerScore} to ${computerScore}...`
+        playerScore = 0;
+        computerScore = 0;
     }
 }
-*/
+
+
+let playerScore = 0;
+let computerScore = 0;
 const buttons = document.querySelectorAll(".choice");
 const results = document.querySelector(".results");
 buttons.forEach(btn => {
-    btn.addEventListener('click', playRound);
+    btn.addEventListener('click', game);
 });
